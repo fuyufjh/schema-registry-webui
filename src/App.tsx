41c97a095b256a8c1174d4bc147885b25605799d
Eric Fu <fuyufjh@gmail.com>
2024-09-18 23:22:00 +0800

    remove the schema page

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,11 @@ import {
 } from "@chakra-ui/react";
 import theme from "./theme";
 import { BrowserRouter as Router, Route, Routes, Link as RouterLink } from "react-router-dom";
-import Schemas from "./components/Schemas";
 import Subjects from "./components/Subjects";
 import Metadata from "./components/Metadata";
 
 const Sidebar = () => (
   <VStack align="stretch" width="200px" p={4} bg="gray.100" height="100vh">
-    <Link as={RouterLink} to="/schemas">Schemas</Link>
     <Link as={RouterLink} to="/subjects">Subjects</Link>
     <Link as={RouterLink} to="/metadata">Metadata</Link>
   </VStack>
@@ -29,7 +27,6 @@ const App: React.FC = () => (
         <Box flex={1}>
           <Box p={4}>
           <Routes>
-              <Route path="/schemas" element={<Schemas />} />
               <Route path="/subjects" element={<Subjects />} />
               <Route path="/metadata" element={<Metadata />} />
               <Route path="/" element={<Text fontSize="2xl">Welcome to Confluent Schema Registry WebUI</Text>} />
